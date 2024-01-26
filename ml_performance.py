4c0eba4d1d9291e3518ce3258a45629688ed1477
Naesars <1771749116@qq.com>
2024-01-26 17:52:35 +0800

    update some examples

--- ml_performance.py
+++ ml_performance.py
@@ -11,8 +11,102 @@ from tqdm import tqdm, trange
 from data_utils import DataLoader
 
 
+class FastMLFactorEvaluator:
+    '''减少了数据使用，便于快速测试因子效果'''
+    def __init__(self, method, record_names:list, factor_names:list, params_dict={}):
+        assert len(record_names) == len(factor_names), 'record_names和factor_names长度不一致'
+        self.record_names = record_names
+        self.factor_dict = dict(zip(record_names, factor_names))
+        self.record_df = pd.DataFrame()
+        self.method = method
+        if len(params_dict) == 0:
+            self.params_dict = {'n_jobs': -1}
+        else:
+            self.params_dict = params_dict
+
+    def evaluate(self, dataloader:DataLoader, y_col, subset_data=0.1):
+        '''
+        评估所有的因子
+        :param dataloader: DataLoader
+        :param y_col: y的列名
+        :param subset_data: 是否只评估部分数据，如果是，那么按照subset比例随机选取数据
+
+        '''
+        valid_year_month = dataloader.get_valid_year_month()
+        subset_num = int(len(valid_year_month) * subset_data)
+        ind = np.random.choice(range(len(valid_year_month)), subset_num, replace=False)
+        self.valid_year_month = [valid_year_month[i] for i in ind]
+        # 划分数据集
+        train_length = int(0.7 * len(self.valid_year_month))
+        train_ind = np.arange(train_length)
+        test_ind = np.arange(train_length, len(self.valid_year_month))
+        # 获取数据
+        train_df = self.get_data(dataloader, train_ind)
+        test_df = self.get_data(dataloader, test_ind)
+        y_train = train_df[y_col]
+        y_test = test_df[y_col]
+
+        auc_list = []
+        f1_list = []
+        # 评估每一个因子
+        for record_name in tqdm(self.record_names):
+            factor_name = self.factor_dict[record_name]  # 取出对应的因子名
+            x_train = train_df[factor_name]
+            x_test = test_df[factor_name]
+            # 训练并评估
+            f1, auc_score = self.train_and_evaluate(x_train, y_train, x_test, y_test)
+            f1_list.append(f1)
+            auc_list.append(auc_score)
+        # 记录结果
+        self.record_df['record_names'] = self.record_names
+        self.record_df['f1'] = f1_list
+        self.record_df['auc'] = auc_list
+
+    def get_data(self, dataloader:DataLoader, indices):
+        '''
+        通过idx 读取对应的月份的数据
+        :param dataloader: DataLoader
+        :param indices: 训练集索引，或者测试集索引
+        :return:
+        '''
+        valid_year_month = self.valid_year_month
+        df_list = []
+        for ind in indices:
+            year, month = valid_year_month[ind]
+            temp_df_list = dataloader.get_month_df(year, month)
+            df_list += temp_df_list  # 合并为一个更长的list，每一个元素都是一个df
+        data_df = pd.concat(df_list, axis=0)
+        data_df = data_df[np.abs(data_df['close_Z']) > 1.4]  # 默认的cut
+        data_df.dropna(inplace=True)
+        data_df = data_df.reset_index(drop=True)
+        return data_df
+
+    def train_and_evaluate(self, x_train, y_train, x_test, y_test):
+        '''
+            单次评估模型返回评估结果
+        '''
+        model = self.method(**self.params_dict)
+        # 模型训练
+        model.fit(x_train, y_train)
+        Y_pred = model.predict(x_test)
+        f1 = f1_score(y_test, Y_pred)
+        auc_score = auc(y_test, Y_pred)
+        return f1, auc_score
+
+    def get_assess_df(self, metric='f1'):
+        '''
+        获取评估结果的df，没想好怎么写更方便，暂时先能够按照f1，auc的格式输出
+        :return:
+        '''
+        assess_df = pd.DataFrame()
+        for record_name in self.record_names:
+            temp_record = self.record_dict[record_name][metric]
+            assess_df[record_name] = temp_record
+        return assess_df
+
+
 class MLFactorEvaluator:
-    def __init__(self, method, record_names:list, factor_names:list, cv=10, params_dict={}):
+    def __init__(self, method, record_names:list, factor_names:list, cv=5, params_dict={}):
         assert len(record_names) == len(factor_names), 'record_names和factor_names长度不一致'
         self.record_names = record_names
         self.factor_dict = dict(zip(record_names, factor_names))
@@ -20,16 +114,28 @@ class MLFactorEvaluator:
         self.cv = cv
         self.method = method
         if len(params_dict) == 0:
-            self.params_dict = {'max_depth': 5}
+            self.params_dict = { 'n_jobs': -1}
         else:
             self.params_dict = params_dict
 
 
-    def evaluate(self, dataloader:DataLoader, y_col):
+    def evaluate(self, dataloader:DataLoader, y_col, subset_data=1):
         '''
         评估所有的因子
+        :param dataloader: DataLoader
+        :param y_col: y的列名
+        :param subset_data: 是否只评估部分数据，如果是，那么按照subset比例随机选取数据
+
         '''
-        self.valid_year_month = dataloader.get_valid_year_month()  # 有效的年月列表
+        valid_year_month = dataloader.get_valid_year_month()  # 有效的年月列表
+        if subset_data < 1:
+            # 随机选取部分数据
+            subset_num = int(len(valid_year_month) * subset_data)
+            ind = np.random.choice(range(len(valid_year_month)), subset_num, replace=False)
+            self.valid_year_month = [valid_year_month[i] for i in ind]
+        else:
+            self.valid_year_month = valid_year_month
+
         # 进行时序cv划分
         self.train_test_indices = self.time_series_split(len(self.valid_year_month))   # train_test_indices = [(train_ind, test_ind), ...]
         # 评估每一个因子
@@ -61,7 +167,7 @@ class MLFactorEvaluator:
         :return:
             [(train_ind, test_ind), ...]
         '''
-        tscv = TimeSeriesSplit(n_splits=self.cv, max_train_size=12, test_size=3)
+        tscv = TimeSeriesSplit(n_splits=self.cv, max_train_size=24, test_size=3)
         X = np.ones(time_length)
         train_test_indices = [index for index in tscv.split(X)]
         return train_test_indices
@@ -80,8 +186,9 @@ class MLFactorEvaluator:
             temp_df_list = dataloader.get_month_df(year, month)
             df_list += temp_df_list  # 合并为一个更长的list，每一个元素都是一个df
         data_df = pd.concat(df_list, axis=0)
-        data_df = data_df[np.abs(data_df['close_Z']) > 1.07]  # 默认的cut
+        data_df = data_df[np.abs(data_df['close_Z']) > 1.4]  # 默认的cut
         data_df.dropna(inplace=True)
+        data_df = data_df.reset_index(drop=True)
         return data_df
 
     def train_and_evaluate(self, x_train, y_train, x_test, y_test):
@@ -95,7 +202,6 @@ class MLFactorEvaluator:
         f1 = f1_score(y_test, Y_pred)
         auc_score = auc(y_test, Y_pred)
         return f1, auc_score
-
 
     def get_assess_df(self, metric='f1'):
         '''
--- test.py
+++ test.py
@@ -1,61 +1,9 @@
-import alphalens as al
-
+import pandas_ta as ta
 import pandas as pd
-from pandas_ta.core import rsi
+from pandas_ta import core
+e = pd.DataFrame()
 
-def read_df(path):
-    '''
-    读取单个数据文档
-    '''
-    binance_price_df = pd.read_csv(path)
-    binance_price_df.columns = ['open_time','open','high','low','close','volume','close_time','quote_volume','count',
-                                'taker_buy_volume','taker_buy_quote_volume','ignore']
-    return binance_price_df
+core_indicators = e.ta.indicators(as_list=True)
+fun = getattr(core, core_indicators[0])
 
-
-def set_datetime_index(data_df, colname='open_time'):
-    data_df['DateTime'] = pd.to_datetime(data_df[colname], unit='ms')
-    data_df = data_df.set_index('DateTime')
-    return data_df
-
-df1 = read_df('../data/15m_data/BTC2020/BTCUSDT-15m-2020-01.zip')
-df1 = set_datetime_index(df1)
-
-price_1 = pd.DataFrame(rsi(df1['close']))
-price_1.columns = ['BTC']
-
-df2 = read_df('../data/15m_data/ETH2020/ETHUSDT-15m-2020-01.zip')
-df2 = set_datetime_index(df1)
-
-price_2 = pd.DataFrame(rsi(df1['close']))
-price_2.columns = ['ETH']
-
-a = price_1.copy()
-a['price'] = a['BTC']
-a['BTC'] = 'BTC'
-a.rename({'BTC':'coin'},axis=1, inplace=True)
-
-b = price_2.copy()
-b['price'] = b['ETH']
-b['ETH'] = 'ETH'
-b.rename({'ETH':'coin'},axis=1, inplace=True)
-
-df = pd.concat([a,b])
-df.reset_index(inplace=True)
-factor_df = df.set_index(keys=['DateTime', 'coin'])
-factor_df.sort_index(inplace=True)
-
-btc_df = df1[['close']]
-btc_df.columns = ['BTC']
-
-eth_df = df2[['close']]
-eth_df.columns = ['ETH']
-
-price_df = pd.concat([btc_df, eth_df], axis=1)
-price_df.sort_index(inplace=True)
-
-factor_df.index.levels[0].freq = '15T'
-price_df.index.freq = '15T'
-
-
-al.utils.get_clean_factor_and_forward_returns(factor_df, price_df)
+e.ta.PVOL()
